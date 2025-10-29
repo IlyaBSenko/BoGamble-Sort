e9620c2c5e0fa7a3ba8c16a0dd83fed0afd63e1e
Ilya Balatsenko <balatsir@dukes.jmu.edu>
2025-10-29 10:21:13 -0400

    Added gui stuff, doesnt work but wtvr, also added gitginore

--- gui.java
+++ gui.java
@@ -1,3 +1,29 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Button;
+
+// add button to start bogamble sort
+// add timer to count how much time it takes to sort using bogamble sort
+// add entry button to gamble how much time it would take (you win if you get close within the seconds???)
+// add different length sizes to sort
+
 public class gui {
+    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("BoGamble Sort!");
+        frame.setBackground(Color.ORANGE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+    
+
+    
+        
+        JLabel label = new JLabel("Hello placeholder");
+        frame.getContentPane().add(label);
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+    }
     
 }
